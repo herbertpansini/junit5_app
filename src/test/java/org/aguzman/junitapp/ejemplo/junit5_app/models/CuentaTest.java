d34d66f7c9f29c6cb3957598ebce3e005fe4cb88
Herbert <hebert.aggrandize@fornecedores.klabin.com.br>
2022-06-21 10:18:56 -0300

    Escribiendo test para el saldo.

--- src/test/java/org/aguzman/junitapp/ejemplo/junit5_app/models/CuentaTest.java
+++ src/test/java/org/aguzman/junitapp/ejemplo/junit5_app/models/CuentaTest.java
@@ -16,4 +16,12 @@ class CuentaTest {
         assertEquals(esperado, real);
         assertTrue(real.equals(esperado));
     }
+
+    @Test
+    void testSaldoCuenta() {
+        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
+        assertEquals(1000.12345, cuenta.getSaldo().doubleValue());
+        assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
+        assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
+    }
 }
