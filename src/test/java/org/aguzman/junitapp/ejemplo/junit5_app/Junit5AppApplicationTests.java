0b98ea1ab00a8fd25866ef2d0f9a7cd8f2182db9
Herbert <hebert.aggrandize@fornecedores.klabin.com.br>
2022-06-21 09:24:13 -0300

    Initial commit.

---
+++ src/test/java/org/aguzman/junitapp/ejemplo/junit5_app/Junit5AppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aguzman.junitapp.ejemplo.junit5_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
